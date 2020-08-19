ef624bd434cce0669a95a6855396debccc1baa05
nerrorsec <42860825+nerrorsec@users.noreply.github.com>
2020-08-19 05:17:59 -0600

    Update ct-is.js

--- test/ct-is.js
+++ test/ct-is.js
@@ -1,7 +1,11 @@
 
-window.addEventListener(“message”, handler, true);
+window.addEventListener("message", handler, true);
 function handler(event) {
 if(event.origin === ‘chat.nirajkhatiwada.com.np’) {
   document.write('No XSS here')
 } else {
   alert('Hi! L0L')
+
+  
+  iframe1.contentWindow.postMessage(“Hello world”,”http://
+www.example.com”);
