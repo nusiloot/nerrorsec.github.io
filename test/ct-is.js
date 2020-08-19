d8a26458061300b161092195c6f454b422d9192b
nerrorsec <42860825+nerrorsec@users.noreply.github.com>
2020-08-19 05:11:15 -0600

    Update ct-is.js

--- test/ct-is.js
+++ test/ct-is.js
@@ -1,1 +1,7 @@
 
+window.addEventListener(“message”, handler, true);
+function handler(event) {
+if(event.origin === ‘chat.nirajkhatiwada.com.np’) {
+  document.write('No XSS here')
+} else {
+  alert('Hi! L0L')
